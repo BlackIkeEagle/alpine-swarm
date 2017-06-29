89e97919de42577229d06bb5cf3f029c65cff9ea
BlackEagle <ike.devolder@gmail.com>
2017-06-29 11:24:22 +0200

    remove the need to restart the nodes to have working docker and allow vagrant user to access docker
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- init-swarm.sh
+++ init-swarm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-vagrant ssh node-1 -- "sudo docker swarm init --advertise-addr=192.168.254.31"
-token=$(vagrant ssh node-1 -- "sudo docker swarm join-token -q worker")
-vagrant ssh node-2 -- "sudo docker swarm join --token $token 192.168.254.31"
-vagrant ssh node-3 -- "sudo docker swarm join --token $token 192.168.254.31"
+vagrant ssh node-1 -- "docker swarm init --advertise-addr=192.168.254.31"
+token=$(vagrant ssh node-1 -- "docker swarm join-token -q worker")
+vagrant ssh node-2 -- "docker swarm join --token $token 192.168.254.31"
+vagrant ssh node-3 -- "docker swarm join --token $token 192.168.254.31"
