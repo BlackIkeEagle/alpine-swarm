d4f2e09905e54ad871a2108ccb8945c8b279232f
BlackEagle <ike.devolder@gmail.com>
2018-10-02 20:50:41 +0200

    add portainer application with agents
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

---
+++ applications/portainer/deploy.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+docker stack deploy --compose-file=portainer.yml portainer
---
+++ applications/portainer/init.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+vagrant ssh storage-1 -- 'sudo mkdir -p /srv/storage/portainer'
+
