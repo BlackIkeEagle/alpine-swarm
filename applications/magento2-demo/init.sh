848fbea871c7ee49d216fb6fe85322adf950e15b
BlackEagle <ike.devolder@gmail.com>
2018-10-03 07:19:41 +0200

    only use storage-1 ssh config for magento init
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- applications/magento2-demo/init.sh
+++ applications/magento2-demo/init.sh
@@ -5,7 +5,7 @@ vagrant ssh storage-1 -- 'sudo mkdir -p /srv/storage/magento2/redis'
 vagrant ssh storage-1 -- 'cd /srv/storage/magento2/; sudo wget -c https://github.com/BlackIkeEagle/swarm-sample-magento2/releases/download/0.0.0/media.tar.gz'
 vagrant ssh storage-1 -- 'cd /srv/storage/magento2/; sudo wget -c https://github.com/BlackIkeEagle/swarm-sample-magento2/releases/download/0.0.0/magento2-demo.sql'
 vagrant ssh storage-1 -- 'cd /srv/storage/magento2/; sudo tar -zxf media.tar.gz; sudo rm media.tar.gz'
-vagrant ssh-config > tmpsshconfig 2>/dev/null
+vagrant ssh-config storage-1 > tmpsshconfig 2>/dev/null
 scp -F tmpsshconfig env.php storage-1:env.php
 rm tmpsshconfig
 vagrant ssh storage-1 -- 'cd /srv/storage/magento2/; sudo mv /home/vagrant/env.php ./'
