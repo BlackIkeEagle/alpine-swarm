342d1e62327ea76217e357db21e76d7cd77c69b7
BlackEagle <ike.devolder@gmail.com>
2018-10-05 11:15:18 +0200

    magento2-demo: use nfs with docker volume plugin and init the volumes on the storage
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- applications/magento2-demo/init.sh
+++ applications/magento2-demo/init.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
-vagrant ssh storage-1 -- 'sudo mkdir -p /srv/storage/magento2/var'
 vagrant ssh storage-1 -- 'sudo mkdir -p /srv/storage/magento2/redis'
+vagrant ssh storage-1 -- 'sudo mkdir -p /srv/storage/magento2/mysql'
+vagrant ssh storage-1 -- 'sudo mkdir -p /srv/storage/magento2/var'
 vagrant ssh storage-1 -- 'cd /srv/storage/magento2/; sudo wget -c https://github.com/BlackIkeEagle/swarm-sample-magento2/releases/download/0.0.0/media.tar.gz'
 vagrant ssh storage-1 -- 'cd /srv/storage/magento2/; sudo wget -c https://github.com/BlackIkeEagle/swarm-sample-magento2/releases/download/0.0.0/magento2-demo.sql'
 vagrant ssh storage-1 -- 'cd /srv/storage/magento2/; sudo tar -zxf media.tar.gz; sudo rm media.tar.gz'
@@ -9,7 +10,5 @@ vagrant ssh-config storage-1 > tmpsshconfig 2>/dev/null
 scp -F tmpsshconfig env.php storage-1:env.php
 rm tmpsshconfig
 vagrant ssh storage-1 -- 'cd /srv/storage/magento2/; sudo mv /home/vagrant/env.php ./'
-vagrant ssh storage-1 -- 'cd /srv/storage/magento2/; sudo chown -R 33:33 var media env.php; sudo chown -R 100:101 redis'
-
-vagrant ssh manager-2 -- 'sudo mkdir /var/lib/magento2-mysql; sudo chown 999:999 /var/lib/magento2-mysql'
+vagrant ssh storage-1 -- 'cd /srv/storage/magento2/; sudo chown -R 33:33 var media env.php; sudo chown -R 100:101 redis; sudo chown -R 999:999 mysql'
 
